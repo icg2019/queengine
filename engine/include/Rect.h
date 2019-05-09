3ed9637292b9d2075335a19294cd486c325fe7e3
alaxalves <alaxallves@gmail.com>
2019-05-09 15:08:52 -0300

    Adds Rectangle class
    Co-authored-by: ThalissonMelo <tallisonmelo46@gmail.com>
    Co-authored-by: Matheus Richard <matheusrichardt@gmail.com>
    Co-authored-by: Matheus Bernardo <matheussbernardo@gmail.com>
    Co-authored-by: Lucas Martins <lucas465@hotmail.com>

---
+++ engine/include/Rect.h
@@ -0,0 +1,22 @@
+#ifndef RECT_H
+#define RECT_H
+
+#include "Vec2.h"
+
+class Rect {
+public:
+  float x, y, w, h;
+
+  Rect();
+  Rect(float x, float y, float w, float h);
+  bool Contains(float x, float y);
+  Vec2 GetPos();
+  Vec2 GetCenter();
+  void SetPos(float x, float y);
+  void SetPos(Vec2 pos);
+  void UpdatePos(Vec2 pos);
+  void SetCenterPos(float centerX, float centerY);
+  void SetCenterPos(Vec2 center);
+};
+
+#endif
