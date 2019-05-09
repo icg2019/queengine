32eacc2f42f680f96cb9c54bc21709d70dadf680
Lucas Martins <lucas465@hotmail.com>
2019-05-09 15:12:54 -0300

    Adds MathHelper module
    Co-authored-by: ThalissonMelo <tallisonmelo46@gmail.com>
    Co-authored-by: Matheus Richard <matheusrichardt@gmail.com>
    Co-authored-by: alaxalves <alaxallves@gmail.com>
    Co-authored-by: Matheus Bernardo <matheussbernardo@gmail.com>

---
+++ engine/src/MathHelper.cpp
@@ -0,0 +1,17 @@
+#include "MathHelper.h"
+#include <iostream>
+
+bool Math::Equals(float a, float b) {
+  return (abs(a - b) <= MINIMUM_DIFFERENCE);
+}
+float Math::Radians(float degree) {
+  return (degree * (PI / 180.0));
+}
+
+float Math::Degrees(float radians) {
+  return (radians * (180.0 / PI));
+}
+
+float Math::GetRand(float min, float max) {
+  return min + static_cast<float> (rand()) / (static_cast<float>(RAND_MAX / (max - min)));
+}
