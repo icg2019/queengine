ef130f027fa8f6ccd8c07728b2b6ab28990bd1ab
rodrigocam <rodrigo.redcode@gmail.com>
2019-05-14 13:09:10 -0300

    Update Bufferset

--- engine/include/BufferSet.hpp
+++ engine/include/BufferSet.hpp
@@ -1,29 +1,23 @@
-#ifndef BUFFER_SET
-#define BUFFER_SET
-
-#define VERTEX_BUFFER 0
-#define TEXTURE_BUFFER 1
-#define ELEMENT_BUFFER 2
+#pragma once
 
 #include <vector>
 
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 
-#include "Buffers.hpp"
+#include "Buffer.hpp"
 
 class BufferSet {
 
     private:
         GLuint id;
         GLuint program;
-        std::vector<BufferInterface> buffers;
+        std::vector<Buffer> buffers;
 
     public:
         BufferSet(GLuint program);
         ~BufferSet();
-        void add(int type, void* data);
+        
+        template <class T>
+        void add(T *data);
 };
-
-
-#endif
