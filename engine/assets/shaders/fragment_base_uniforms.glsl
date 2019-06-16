b0801088069e14752f4884048be21d55a0f4ad08
Guilherme Augusto <guilherme.francais@gmail.com>
2019-06-15 23:30:39 -0300

    Creates a file to store all default uniforms
    Co-authored-by: Bruna Pinos <bruna.pinos@gmail.com>
    Co-authored-by: Clarissa Borges <clarissalimab08@gmail.com>
    Co-authored-by: Guilherme Marques <marquesguilherme3@gmail.com>
    Co-authored-by: lucasssm <lucas50xd@hotmail.com>

---
+++ engine/assets/shaders/fragment_base_uniforms.glsl
@@ -0,0 +1,32 @@
+#version 300 es
+
+precision highp float;
+precision highp int;
+precision highp sampler2D;
+
+uniform vec3      iResolution;                  // viewport resolution (in pixels)
+uniform float     iTime;                        // shader playback time (in seconds)
+uniform float     iGlobalTime;                  // shader playback time (in seconds)
+uniform vec4      iMouse;                       // mouse pixel coords
+uniform vec4      iDate;                        // (year, month, day, time in seconds)
+uniform float     iSampleRate;                  // sound sample rate (i.e., 44100)
+uniform vec3      iChannelResolution[4];        // channel resolution (in pixels)
+uniform float     iChannelTime[4];              // channel playback time (in sec)
+
+uniform vec2      ifFragCoordOffsetUniform;     // used for tiled based hq rendering
+uniform float     iTimeDelta;                   // render time (in seconds)
+uniform int       iFrame;                       // shader playback frame
+uniform float     iFrameRate;
+
+struct Channel {
+    vec3    resolution;
+    float   time;
+};
+
+uniform Channel iChannel[4];
+
+in vec2 fragCoord;
+in vec2 tex_coord;
+in vec2 v_color;
+
+out vec4 fragColor;
