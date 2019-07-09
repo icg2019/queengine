aec16b309794812830b4810aa40df253816c4108
Victor Moura <victor_cmoura@hotmail.com>
2019-07-09 11:53:45 -0300

    Changes default C arrays to STL vector

--- engine/include/Rectangle.hpp
+++ engine/include/Rectangle.hpp
@@ -17,12 +17,12 @@ class Rectangle {
 			std::vector<glm::vec3> coordinates
 		);
 
-		float* get_coordinates();
+		std::vector<float> get_coordinates();
 		void set_coordinates(std::vector<glm::vec3>);
 
         unsigned int get_coordinates_size();
 
-		unsigned int* get_indices();
+		std::vector<unsigned int> get_indices();
 		void set_indices(std::vector<glm::vec1>);
 
         unsigned int get_indices_size();
