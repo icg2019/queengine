11e8d1b9630faa5d23a92398092f43ee54812e5f
Yoshida-Eduardo <edry97@hotmail.com>
2019-07-11 00:55:42 -0300

    work in progress, trying to send materials to shaders

--- engine/assets/shaders/fragment_from_texture.glsl
+++ engine/assets/shaders/fragment_from_texture.glsl
@@ -5,6 +5,13 @@ in vec3 Normal_cameraspace;
 in vec3 EyeDirection_cameraspace;
 in vec3 LightDirection_cameraspace;
 
+//obj materials
+
+//uniform vec3 obj_ambient;
+//uniform vec3 obj_specular;
+//uniform vec3 obj_diffuse;
+
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord){
 
 	// Light emission properties
@@ -12,11 +19,11 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord){
 	vec3 LightColor = vec3(1.0, 1.0, 1.0);
 	float LightPower = 150.0f;
 	vec3 LightPosition_worldspace = vec3(4.0f, 4.0f, 4.0f);
-	
+
 	// Material properties
 	vec3 MaterialDiffuseColor = texture(iChannel0, fragCoord ).rgb;
-	vec3 MaterialAmbientColor = vec3(0.1,0.1,0.1) * MaterialDiffuseColor;
-	vec3 MaterialSpecularColor = vec3(0.3,0.3,0.3);
+	vec3 MaterialAmbientColor = vec3(0.1f, 0.1f, 0.1f) * MaterialDiffuseColor;
+	vec3 MaterialSpecularColor = vec3(0.5f, 0.3f, 0.5f);
 
 	// Distance to the light
 	float distance = length( LightPosition_worldspace - Position_worldspace );
@@ -25,13 +32,13 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord){
 	vec3 n = normalize( Normal_cameraspace );
 	// Direction of the light (from the fragment to the light)
 	vec3 l = normalize( LightDirection_cameraspace );
-	// Cosine of the angle between the normal and the light direction, 
+	// Cosine of the angle between the normal and the light direction,
 	// clamped above 0
 	//  - light is at the vertical of the triangle -> 1
 	//  - light is perpendicular to the triangle -> 0
 	//  - light is behind the triangle -> 0
 	float cosTheta = clamp( dot( n,l ), 0.0f, 1.0f );
-	
+
 	// Eye vector (towards the camera)
 	vec3 E = normalize(EyeDirection_cameraspace);
 	// Direction in which the triangle reflects the light
@@ -41,7 +48,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord){
 	//  - Looking into the reflection -> 1
 	//  - Looking elsewhere -> < 1
 	float cosAlpha = clamp( dot( E,R ), 0.0f, 1.0f);
-	
+
 	fragColor =
 		// Ambient : simulates indirect lighting
 		vec4(MaterialAmbientColor +
